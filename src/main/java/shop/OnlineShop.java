38ed857ed82350b09958e7377bec64532a90c302
Darius B. <73719745+Dariuz123@users.noreply.github.com>
2025-07-09 14:55:09 +0200

    addtocart update

--- src/main/java/shop/OnlineShop.java
+++ src/main/java/shop/OnlineShop.java
@@ -27,7 +27,7 @@ public class OnlineShop {
     }
 
     public void addToCart(Person buyer, ShopItem item){
-        // TODO: Add the item to the buyer's cart. If the buyer doesn't have a cart yet, create one.
+            bankAccounts.put(person, new BankAccount(person, initialDeposit));
     }
 
     public boolean placeOrder(Person buyer) {
